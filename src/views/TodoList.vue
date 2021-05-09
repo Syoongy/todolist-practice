e6fe8a109e8422e83cac1766baa32a9f65dabad2
1st2GetThisName <83216938+1st2GetThisName@users.noreply.github.com>
2021-05-09 22:26:29 +0800

    Added CSS Still haven't put a link from Home to Todo tho

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,15 +1,25 @@
 <template>
     <main>
         <div class="bg-gray-50">
-            <div id="bod">
-                <ol>
+            <div id="bod" class="px-2">
+                <ol class="list-decimal list-inside">
                     <li v-for="item in items">{{item.todo}}</li>
                 </ol>
-                <div>
-                    <input type="text"  v-model="toadd" />
-                    <input type="button" value="Add item" v-on:click="additem" /><br>
-                    <input type="number" v-model="toremove" />
-                    <input type="button" value="Remove item" v-on:click="removeitem" />
+                <div class="grid grid-cols-5 gap-2 py-2.5">
+                    <div class="col-span-4">
+                        <input type="text" v-model="toadd"
+                               class="text-sm appearance-none rounded w-full px-3 text-gray-700 bg-gray-200 leading-tight border-none focus:outline-none focus:ring-blue-300 focus:ring-4 h-10" />
+                    </div>
+                    <div>
+                        <input type="button" value="Add item" v-on:click="additem" class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg w-full" />
+                    </div>
+                    <div class="col-span-4">
+                        <input type="number" v-model="toremove"
+                                class="text-sm appearance-none rounded w-full px-3 text-gray-700 bg-gray-200 leading-tight border-none focus:outline-none focus:ring-blue-300 focus:ring-4 h-10" />
+                    </div>
+                    <div>
+                        <input type="button" value="Remove item" v-on:click="removeitem" class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg w-full" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -39,6 +49,7 @@
             window.alert("Please enter the index of a todo item.");
             return;
         };
+        toremove.value = 0;
         items.splice(x, 1);
         toremove.value = 1; //ok wtf why do I need to do this to make the list update immediately
     };
